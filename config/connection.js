480fcdb9a4dd1fad6e1f789f9fb2af6420245aee
Carol Giblin <giblinCK@gmail.com>
2018-05-22 21:22:49 -0600

    heroku is stupid

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 //require and connect to mysql database
 const mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL) {
+connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+}
 
-var connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "burgers_db"
-});
+
 
 
 connection.connect(function (err) {
